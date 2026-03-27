719472b866ac1a2edb3b9d3f0382d2dfb1ff3bc5
Alexis Kypridemos <alexiskypridemos@yahoo.com>
2026-03-27 17:43:39 +0200

    TG Admins custom validation - FR validation messages

--- modules/validation/language/fr/validation_errors.php
+++ modules/validation/language/fr/validation_errors.php
@@ -44,5 +44,9 @@ $validation_errors = [
     'upload_failed_error'        => 'Le téléchargement de [label] a échoué. Veuillez réessayer.',
     'not_an_image_error'         => 'Le fichier [label] doit être une image valide (JPG, PNG, GIF ou WEBP).',
     'invalid_file_error'         => 'Le fichier [label] est invalide ou corrompu.',
-    'security_threat_error'      => 'Une menace de sécurité a été détectée dans le fichier [label]. Le téléchargement a été bloqué.'
+    'security_threat_error'      => 'Une menace de sécurité a été détectée dans le fichier [label]. Le téléchargement a été bloqué.',
+	
+	// Erreurs des Administrateurs Trongate
+	'username_taken'             => 'Le [label] est déjà pris. Veuillez en choisir un autre.',
+	'username_in_use'            => 'Le [label] est déjà utilisé par un autre compte.'
 ];
